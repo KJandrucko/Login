d07512ca1c7a8cb96a2186cd6116c0b7693912c7
Kaitlyn Jandrucko <KaitlynJandrucko@kaitlyns-macbook-pro.local>
2019-04-02 22:33:38 -0400

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Kaitlyn Jandrucko on 4/2/19.
+//  Copyright © 2019 Kaitlyn Jandrucko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
